11ee436c5d8b67627e17dce2e8527ac8fca1f8d4
Danni Liu <danni.liu@maastrichtuniversity.nl>
2020-11-24 18:25:27 +0100

    Fixed bug in binarize-data.sh

--- tatoeba/binarize-data.sh
+++ tatoeba/binarize-data.sh
@@ -6,7 +6,7 @@ BIN_DATA_DIR=$BASE_DIR/data/bpe_${BPESIZE}_bin
 mkdir $BIN_DATA_DIR -p
 
 SHARED_DICT=$BIN_DATA_DIR/dict.txt
-
+if false; then
 # combine all sentences to create a joint vocab
 for f in $PREPRO_DIR/train/*; do
 	cat $f >> $PREPRO_DIR/train/tmp.src
@@ -28,28 +28,25 @@ rm $PREPRO_DIR/train/tmp.src $PREPRO_DIR/train/tmp.trg
 # point to shared dictionary for reusing later
 # use the same shared dictionary for all language pairs
 ln -s $BIN_DATA_DIR/dict.src.txt $SHARED_DICT
-
+fi
 # We split train/dev/test binarization in three steps due to possibly missing data
 # (e.g. there is training data for one direction but no data in dev/test).
 
 # binarize training set (en-X)
-for f in $PREPRO_DIR/train/*eng*\.{src,trg}; do
+for f in $PREPRO_DIR/train/*eng*\.src; do
 	fname="${f##*/}"
 	lan_pair="${fname%.*}"
+	lan_pair_path="${f%.*}"
 
-	echo '*** Binarizing direction:' $lan_pair
+	echo '*** Binarizing direction (train):' $lan_pair
 	src_lang=$(echo $lan_pair | cut -f1 -d-) #(${lan_pair//-/ })
 	trg_lang=$(echo $lan_pair | cut -f2 -d-)
 	
 	# prepare pointers to fit fairseq-preprocess
-	if [[ "$f" == *src ]]; then
-		ln -s $f $PREPRO_DIR/train/$src_lang-$trg_lang.$src_lang
-		ln -s $f $PREPRO_DIR/train/$trg_lang-$src_lang.$src_lang
-		
-	else
-		ln -s $f $PREPRO_DIR/train/$src_lang-$trg_lang.$trg_lang
-		ln -s $f $PREPRO_DIR/train/$trg_lang-$src_lang.$trg_lang
-	fi
+	ln -s $f $PREPRO_DIR/train/$src_lang-$trg_lang.$src_lang
+	ln -s $f $PREPRO_DIR/train/$trg_lang-$src_lang.$src_lang
+	ln -s $lan_pair_path.trg $PREPRO_DIR/train/$src_lang-$trg_lang.$trg_lang
+	ln -s $lan_pair_path.trg $PREPRO_DIR/train/$trg_lang-$src_lang.$trg_lang
 
 	# src --> trg
 	fairseq-preprocess \
@@ -76,23 +73,20 @@ for f in $PREPRO_DIR/train/*eng*\.{src,trg}; do
 done
 
 # binarize dev set
-for f in $PREPRO_DIR/dev/*\.{src,trg}; do
+for f in $PREPRO_DIR/dev/*\.src; do
         fname="${f##*/}"
         lan_pair="${fname%.*}"
+	lan_pair_path="${f%.*}"
 
-        echo '*** Binarizing direction:' $lan_pair
+	echo '*** Binarizing direction (dev):' $lan_pair
         src_lang=$(echo $lan_pair | cut -f1 -d-) #(${lan_pair//-/ })
         trg_lang=$(echo $lan_pair | cut -f2 -d-)
 
         # prepare pointers to fit fairseq-preprocess
-        if [[ "$f" == *src ]]; then
-                ln -s $f $PREPRO_DIR/dev/$src_lang-$trg_lang.$src_lang
-                ln -s $f $PREPRO_DIR/dev/$trg_lang-$src_lang.$src_lang
-
-        else
-                ln -s $f $PREPRO_DIR/dev/$src_lang-$trg_lang.$trg_lang
-                ln -s $f $PREPRO_DIR/dev/$trg_lang-$src_lang.$trg_lang
-        fi
+        ln -s $f $PREPRO_DIR/dev/$src_lang-$trg_lang.$src_lang
+        ln -s $f $PREPRO_DIR/dev/$trg_lang-$src_lang.$src_lang
+	ln -s $lan_pair_path.trg $PREPRO_DIR/dev/$src_lang-$trg_lang.$trg_lang
+        ln -s $lan_pair_path.trg $PREPRO_DIR/dev/$trg_lang-$src_lang.$trg_lang
 
         fairseq-preprocess \
                 --source-lang $src_lang --target-lang $trg_lang \
@@ -118,26 +112,23 @@ for f in $PREPRO_DIR/dev/*\.{src,trg}; do
 done
 
 # binarize test set
-for f in $PREPRO_DIR/test/*\.{src,trg}; do
+for f in $PREPRO_DIR/test/*\.src; do
         fname="${f##*/}"
         lan_pair="${fname%.*}"
-
+	lan_pair_path="${f%.*}"
+	
 	if [[ $(wc -l <$f) -le 1000 ]]; then
 		echo '*** Skipping test direction' $lan_pair 'because it has < 1000 sentences.'
 	else
-	        echo '*** Binarizing direction:' $lan_pair
+		echo '*** Binarizing direction (test):' $lan_pair
         	src_lang=$(echo $lan_pair | cut -f1 -d-)
 	        trg_lang=$(echo $lan_pair | cut -f2 -d-)
 
         	# prepare pointers to fit fairseq-preprocess
-	        if [[ "$f" == *src ]]; then
-        	        ln -s $f $PREPRO_DIR/test/$src_lang-$trg_lang.$src_lang
-                	ln -s $f $PREPRO_DIR/test/$trg_lang-$src_lang.$src_lang
-
-	        else
-        	        ln -s $f $PREPRO_DIR/test/$src_lang-$trg_lang.$trg_lang
-                	ln -s $f $PREPRO_DIR/test/$trg_lang-$src_lang.$trg_lang
-	        fi
+        	ln -s $f $PREPRO_DIR/test/$src_lang-$trg_lang.$src_lang
+            	ln -s $f $PREPRO_DIR/test/$trg_lang-$src_lang.$src_lang
+		ln -s $lan_pair_path.trg $PREPRO_DIR/test/$src_lang-$trg_lang.$trg_lang
+	        ln -s $lan_pair_path.trg $PREPRO_DIR/test/$trg_lang-$src_lang.$trg_lang
 
 	        # src --> trg
 	        fairseq-preprocess \
